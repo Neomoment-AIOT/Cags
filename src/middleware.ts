69993ea448fecafc3f11a40b0ace3b5b290e7719
Fstack-MdAltaf <altaf.fstack@gmail.com>
2025-09-17 16:33:49 +0530

    feat: block Turkey in geo-restrictions

--- src/middleware.ts
+++ src/middleware.ts
@@ -92,7 +92,7 @@
 // middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 
-const BLOCKED = new Set(['IN']); // block India
+const BLOCKED = new Set(['PK','TR']); 
 const isProd = process.env.NODE_ENV === 'production';
 
 export const config = {

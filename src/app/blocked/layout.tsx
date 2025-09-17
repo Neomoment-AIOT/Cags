02c95af56af9915a56f00ff50ef6c5dc49404a60
Fstack-MdAltaf <altaf.fstack@gmail.com>
2025-09-17 16:28:47 +0530

    refactor: replace img element with Next.js Image component and increase logo width

--- src/app/blocked/layout.tsx
+++ src/app/blocked/layout.tsx
@@ -16,11 +16,10 @@ export default function BlockedLayout({
                     <div className="container-fluid mx-auto px-4 sm:px-6 lg:px-8">
                         <div className={`flex items-center justify-between h-20 p-2`}>
                             <div className="flex-shrink-0 flex items-center">
-                                <img src="/Logo.png" alt="Logo" className="h-10 mr-4 ml-2 p-1" />
                                 <Image
                                     src="/Logo.png"
                                     alt="CAGS Logo"
-                                    width={150}
+                                    width={200}
                                     height={50}
                                     className="h-10 mr-4 ml-2 p-1" // Replaces the old className
                                     priority
